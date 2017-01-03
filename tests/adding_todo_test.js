0cba06591e6bf200f9d5f5e0dba1b73ff2c346ad
Jakub Płoskonka <ploskonkajakub@gmail.com>
2017-01-03 23:16:23 +0100

    Test adding todo

--- tests/adding_todo_test.js
+++ tests/adding_todo_test.js
@@ -1,5 +1,10 @@
 Feature('Adding todo');
 
 Scenario('User adds a new todo', (I) => {
+  const todoContent = 'Learn testing with CodeceptJS';
 
+  I.amOnPage('/');
+  I.fillField('.new-todo', todoContent);
+  I.pressKey('Enter');
+  I.see(todoContent, '.todo-list');
 });
